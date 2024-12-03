8505f52affe4ddd2aa052444b612502930e3148f
Kevin Noguera <noguera.ke@gmail.com>
2024-12-02 22:30:42 -0500

    initiating functions and style for the ResumePage

---
+++ src/components/ResumePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../style/resume.css';
+
+function Resume() {
+    return (
+      <section>
+        <h2>Resume</h2>
+        <a href="resume.pdf" download>Download Resume</a>
+      <p>
+
+      </p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>MySQL</li>
+        </ul>
+      </section>
+    );
+  }
+  
+  export default Resume;
